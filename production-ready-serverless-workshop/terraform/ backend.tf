8c84bd4b3fa65a976ff42b8155252c4516221a09
TCotton <me@andywalpole.me>
2025-01-25 11:48:53 +0000

    feat(terraform): added dev environment

--- production-ready-serverless-workshop/terraform/ backend.tf
+++ production-ready-serverless-workshop/terraform/ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "workshop-andrew-state-bucket-eu-west-1-922701030910"
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-    dynamodb_table = "workshop-andrew-terraform-state-lock"
-  }
+  backend "s3" {}
 }
--- production-ready-serverless-workshop/terraform/variables.tf
+++ production-ready-serverless-workshop/terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "eu-west-1"
 }
 
 variable "stage_name" {
   description = "Stage name (e.g. dev, prod)"
   type        = string
-  default     = "dev"
 }
 
 variable "service_name" {
   description = "Service name"
   type        = string
-  default     = "workshop-andrew"
 }
