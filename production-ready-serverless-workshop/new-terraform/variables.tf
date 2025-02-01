8b8dc3080a2b8650100f10cd6bf7dc991aed91c4
TCotton <me@andywalpole.me>
2025-02-01 14:07:05 +0000

    chore(terraform): restarted Terraform project

---
+++ production-ready-serverless-workshop/new-terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "stage_name" {
+  description = "Stage name (e.g. dev, prod)"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Service name"
+  type        = string
+}
